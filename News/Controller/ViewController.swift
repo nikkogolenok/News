76a65a7c9687c55e55bcd2b906d0072e9d8bd8a0
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-21 11:21:43 +0300

    Initial Commit

---
+++ News/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Никита Коголенок on 21.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
